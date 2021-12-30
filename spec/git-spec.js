d622423fe22d59b2130af7c32c8b8103a2c08993
Caellion <administrator@michalpol.pl>
2021-12-30 17:50:04 +0100

    fix: linter errors

--- spec/git-spec.js
+++ spec/git-spec.js
@@ -26,7 +26,7 @@ describe('git', () => {
 		gitUrl = await fs.mkdtemp(path.join(os.tmpdir(), 'sync-settings-git-bare-repo-'))
 		await exec('git init --bare', { cwd: gitUrl })
 		atom.config.set('sync-settings-git-location.gitUrl', gitUrl)
-		//atom.config.set('sync-settings-git-location.gitBranch', "master")
+		// atom.config.set('sync-settings-git-location.gitBranch', "master")
 	})
 
 	afterEach(async () => {
